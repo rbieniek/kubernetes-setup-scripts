104a8ae5e511f8088ddbfcbb33c32deb21828d1d
Rainer Bieniek <Rainer.Bieniek@googlemail.com>
2018-09-26 08:57:52 +0200

    Fix commiter

--- generate-certs.sh
+++ generate-certs.sh
@@ -4,12 +4,16 @@ CONTROLLER_NAME=`hostname`
 CONTROLLER_IP=$(getent ahostsv4 $CONTROLLER_NAME|tail -1|awk '{print $1}')
 SERVICE_IP="10.254.0.1"
 
-mkdir -p etc/kubernetes/pki
-mkdir -p etc/etcd/pki
-cd etc/kubernetes/pki
+KUBE_PKI_DIR=etc/kubernetes/pki
+ETCD_PKI_DIR=etc/etcd/pki
+
+
+mkdir -p ${KUBE_PKI_DIR}
+mkdir -p ${ETCD_PKI_DIR}
+
 
 echo "Generate openssl.cnf"
-cat > openssl.cnf << EOF
+cat > ${KUBE_PKI_DIR}/openssl.cnf << EOF
 [ req ]
 distinguished_name = req_distinguished_name
 [req_distinguished_name]
@@ -47,116 +51,126 @@ DNS.1 = ${CONTROLLER_NAME}
 IP.1 = ${CONTROLLER_IP}
 EOF
 
-cp openssl.cnf ../../etcd/pki/
+cp  ${KUBE_PKI_DIR}/openssl.cnf  ${ETCD_PKI_DIR}
 
 echo "Generate Kubernetes CA cert"
-openssl ecparam -name secp521r1 -genkey -noout -out ca.key
-chmod 0600 ca.key
-openssl req -x509 -new -sha256 -nodes -key ca.key -days 3650 -out ca.crt \
-        -subj "/CN=kubernetes-ca"  -extensions v3_ca -config ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out ${KUBE_PKI_DIR}/ca.key
+chmod 0600  ${KUBE_PKI_DIR}/ca.key
+openssl req -x509 -new -sha256 -nodes -key  ${KUBE_PKI_DIR}/ca.key -days 3650 -out  ${KUBE_PKI_DIR}/ca.crt \
+        -subj "/CN=kubernetes-ca"  -extensions v3_ca -config  ${KUBE_PKI_DIR}//openssl.cnf
 
 echo "Generate kube apiserver cert"
-openssl ecparam -name secp521r1 -genkey -noout -out kube-apiserver.key
-chmod 0600 kube-apiserver.key
-openssl req -new -sha256 -key kube-apiserver.key -subj "/CN=kube-apiserver" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out kube-apiserver.crt -days 365 \
-                 -extensions v3_req_apiserver -extfile ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/kube-apiserver.key
+chmod 0600  ${KUBE_PKI_DIR}/kube-apiserver.key
+openssl req -new -sha256 -key  ${KUBE_PKI_DIR}/kube-apiserver.key -subj "/CN=kube-apiserver" \
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/kube-apiserver.crt -days 365 \
+                 -extensions v3_req_apiserver -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate apiserver kubelet client cert"
-openssl ecparam -name secp521r1 -genkey -noout -out apiserver-kubelet-client.key
-chmod 0600 apiserver-kubelet-client.key
-openssl req -new -key apiserver-kubelet-client.key \
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/apiserver-kubelet-client.key
+chmod 0600  ${KUBE_PKI_DIR}/apiserver-kubelet-client.key
+openssl req -new -key  ${KUBE_PKI_DIR}/apiserver-kubelet-client.key \
             -subj "/CN=kube-apiserver-kubelet-client/O=system:masters" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out apiserver-kubelet-client.crt -days 365 \
-                 -extensions v3_req_client -extfile ./openssl.cnf
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/apiserver-kubelet-client.crt -days 365 \
+                 -extensions v3_req_client -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate admin client cert"
-openssl ecparam -name secp521r1 -genkey -noout -out admin.key
-chmod 0600 admin.key
-openssl req -new -key admin.key -subj "/CN=kubernetes-admin/O=system:masters" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out admin.crt -days 365 -extensions v3_req_client \
-                 -extfile ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/admin.key
+chmod 0600  ${KUBE_PKI_DIR}/admin.key
+openssl req -new -key  ${KUBE_PKI_DIR}/admin.key -subj "/CN=kubernetes-admin/O=system:masters" \
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/admin.crt -days 365 -extensions v3_req_client \
+                 -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate Service Account key"
-openssl ecparam -name secp521r1 -genkey -noout -out sa.key
-openssl ec -in sa.key -outform PEM -pubout -out sa.pub
-chmod 0600 sa.key
-openssl req -new -sha256 -key sa.key \
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/sa.key
+openssl ec -in  ${KUBE_PKI_DIR}/sa.key -outform PEM -pubout -out  ${KUBE_PKI_DIR}/sa.pub
+chmod 0600  ${KUBE_PKI_DIR}/sa.key
+openssl req -new -sha256 -key  ${KUBE_PKI_DIR}/sa.key \
             -subj "/CN=system:kube-controller-manager" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out sa.crt -days 365 -extensions v3_req_client \
-                 -extfile ./openssl.cnf
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/sa.crt -days 365 -extensions v3_req_client \
+                 -extfile  ${KUBE_PKI_DIR}/openssl.cnf
+
 
 echo "Generate kube-scheduler cert"
-openssl ecparam -name secp521r1 -genkey -noout -out kube-scheduler.key
-chmod 0600 kube-scheduler.key
-openssl req -new -sha256 -key kube-scheduler.key \
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/kube-scheduler.key
+chmod 0600  ${KUBE_PKI_DIR}/kube-scheduler.key
+openssl req -new -sha256 -key  ${KUBE_PKI_DIR}/kube-scheduler.key \
             -subj "/CN=system:kube-scheduler" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out kube-scheduler.crt -days 365 -extensions v3_req_client \
-                 -extfile ./openssl.cnf
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/kube-scheduler.crt -days 365 -extensions v3_req_client \
+                 -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate front proxy CA cert"
-openssl ecparam -name secp521r1 -genkey -noout -out front-proxy-ca.key
-chmod 0600 front-proxy-ca.key
-openssl req -x509 -new -sha256 -nodes -key front-proxy-ca.key -days 3650 \
-            -out front-proxy-ca.crt -subj "/CN=front-proxy-ca" \
-            -extensions v3_ca -config ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/front-proxy-ca.key
+chmod 0600  ${KUBE_PKI_DIR}/front-proxy-ca.key
+openssl req -x509 -new -sha256 -nodes -key  ${KUBE_PKI_DIR}/front-proxy-ca.key -days 3650 \
+            -out  ${KUBE_PKI_DIR}/front-proxy-ca.crt -subj "/CN=front-proxy-ca" \
+            -extensions v3_ca -config  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate front proxy client cert"
-openssl ecparam -name secp521r1 -genkey -noout -out front-proxy-client.key
-chmod 0600 front-proxy-client.key
-openssl req -new -sha256 -key front-proxy-client.key \
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/front-proxy-client.key
+chmod 0600  ${KUBE_PKI_DIR}/front-proxy-client.key
+openssl req -new -sha256 -key  ${KUBE_PKI_DIR}/front-proxy-client.key \
             -subj "/CN=front-proxy-client" \
-  | openssl x509 -req -sha256 -CA front-proxy-ca.crt \
-                 -CAkey front-proxy-ca.key -CAcreateserial \
-                 -out front-proxy-client.crt -days 365 \
-                 -extensions v3_req_client -extfile ./openssl.cnf
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/front-proxy-ca.crt \
+                 -CAkey  ${KUBE_PKI_DIR}/front-proxy-ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/front-proxy-client.crt -days 365 \
+                 -extensions v3_req_client -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate kube-proxy cert"
-openssl ecparam -name secp521r1 -genkey -noout -out kube-proxy.key
-chmod 0600 kube-proxy.key
-openssl req -new -key kube-proxy.key \
+openssl ecparam -name secp521r1 -genkey -noout -out  ${KUBE_PKI_DIR}/kube-proxy.key
+chmod 0600  ${KUBE_PKI_DIR}/kube-proxy.key
+openssl req -new -key  ${KUBE_PKI_DIR}/kube-proxy.key \
             -subj "/CN=kube-proxy/O=system:node-proxier" \
-  | openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial \
-                 -out kube-proxy.crt -days 365 -extensions v3_req_client \
-                 -extfile ./openssl.cnf
+  | openssl x509 -req -sha256 -CA  ${KUBE_PKI_DIR}/ca.crt -CAkey  ${KUBE_PKI_DIR}/ca.key -CAcreateserial \
+                 -out  ${KUBE_PKI_DIR}/kube-proxy.crt -days 365 -extensions v3_req_client \
+                 -extfile  ${KUBE_PKI_DIR}/openssl.cnf
 
 echo "Generate etcd CA cert"
-openssl ecparam -name secp521r1 -genkey -noout -out etcd-ca.key
-chmod 0600 etcd-ca.key
-openssl req -x509 -new -sha256 -nodes -key etcd-ca.key -days 3650 \
-            -out etcd-ca.crt -subj "/CN=etcd-ca" -extensions v3_ca \
-            -config ./openssl.cnf
-
-cp etcd-ca.crt etcd-ca.key ../../etcd/pki/
+openssl ecparam -name secp521r1 -genkey -noout -out  ${ETCD_PKI_DIR}/ca.key
+chmod 0600  ${ETCD_PKI_DIR}/ca.key
+openssl req -x509 -new -sha256 -nodes -key  ${ETCD_PKI_DIR}/ca.key -days 3650 \
+            -out  ${ETCD_PKI_DIR}/ca.crt -subj "/CN=ca" -extensions v3_ca \
+            -config  ${ETCD_PKI_DIR}/openssl.cnf
 
 echo "Generate etcd cert"
-openssl ecparam -name secp521r1 -genkey -noout -out etcd.key
-chmod 0600 etcd.key
-openssl req -new -sha256 -key etcd.key -subj "/CN=etcd" \
-  | openssl x509 -req -sha256 -CA etcd-ca.crt -CAkey etcd-ca.key \
-                 -CAcreateserial -out etcd.crt -days 365 \
-                 -extensions v3_req_etcd -extfile ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out  ${ETCD_PKI_DIR}/etcd.key
+chmod 0600  ${ETCD_PKI_DIR}/etcd.key
+openssl req -new -sha256 -key  ${ETCD_PKI_DIR}/etcd.key -subj "/CN=etcd" \
+  | openssl x509 -req -sha256 -CA  ${ETCD_PKI_DIR}/ca.crt -CAkey  ${ETCD_PKI_DIR}/ca.key \
+                 -CAcreateserial -out  ${ETCD_PKI_DIR}/etcd.crt -days 365 \
+                 -extensions v3_req_etcd -extfile  ${ETCD_PKI_DIR}/openssl.cnf
 
-cp etcd.key etcd.crt ../../etcd/pki/
+cp ${ETCD_PKI_DIR}/etcd.key ${ETCD_PKI_DIR}/etcd.crt ${KUBE_PKI_DIR}
 
 echo "Generate etcd peer cert"
-openssl ecparam -name secp521r1 -genkey -noout -out etcd-peer.key
-chmod 0600 etcd-peer.key
-openssl req -new -sha256 -key etcd-peer.key -subj "/CN=etcd-peer" \
-  | openssl x509 -req -sha256 -CA etcd-ca.crt -CAkey etcd-ca.key \
-                 -CAcreateserial -out etcd-peer.crt -days 365 \
-                 -extensions v3_req_etcd -extfile ./openssl.cnf
+openssl ecparam -name secp521r1 -genkey -noout -out  ${ETCD_PKI_DIR}/etcd-peer.key
+chmod 0600  ${ETCD_PKI_DIR}/etcd-peer.key
+openssl req -new -sha256 -key  ${ETCD_PKI_DIR}/etcd-peer.key -subj "/CN=etcd-peer" \
+  | openssl x509 -req -sha256 -CA  ${ETCD_PKI_DIR}/ca.crt -CAkey  ${ETCD_PKI_DIR}/ca.key \
+                 -CAcreateserial -out  ${ETCD_PKI_DIR}/etcd-peer.crt -days 365 \
+                 -extensions v3_req_etcd -extfile  ${ETCD_PKI_DIR}/openssl.cnf
 
-cp etcd-peer.key etcd-peer.crt ../../etcd/pki/
+(
+    echo "View Kubernetes certs"
+    cd ${KUBE_PKI_DIR}
+    for i in *crt; do
+	echo $i:;
+	openssl x509 -subject -issuer -noout -in $i;
+	echo;
+    done
+)
 
-echo "View certs"
-for i in *crt; do
-    echo $i:;
-    openssl x509 -subject -issuer -noout -in $i;
-    echo;
-  done
+(
+    echo "View etcd certs"
+    cd ${ETCD_PKI_DIR}
+    for i in *crt; do
+	echo $i:;
+	openssl x509 -subject -issuer -noout -in $i;
+	echo;
+    done
+)
